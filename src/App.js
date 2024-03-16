e5e57bac35e00dec8ada0f95744751a74a371bf3
JacyDee274 <jacydee274@gmail.com>
2024-03-16 21:12:09 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,33 +4,35 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by{" "}
-        <a
-          href="https://github.com/JacyDee274"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Jacky Ilievski{" "}
-        </a>{" "}
-        , is open-sourced on{" "}
-        <a
-          href="https://github.com/JacyDee274/reactweatherappjackyfinal"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub{" "}
-        </a>
-        and hosted on{" "}
-        <a
-          href="https://jacky-weather-app-finale.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="conatainer">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://github.com/JacyDee274"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Jacky Ilievski{" "}
+          </a>{" "}
+          , is open-sourced on{" "}
+          <a
+            href="https://github.com/JacyDee274/reactweatherappjackyfinal"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub{" "}
+          </a>
+          and hosted on{" "}
+          <a
+            href="https://jacky-weather-app-finale.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
