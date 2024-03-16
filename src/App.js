2a1693d2c9aa816fe531963d4efd241c56a370d9
JacyDee274 <jacydee274@gmail.com>
2024-03-16 19:36:10 +0100

    Added Footer Links

--- src/App.js
+++ src/App.js
@@ -2,6 +2,35 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by{" "}
+        <a
+          href="https://github.com/JacyDee274"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Jacky Ilievski{" "}
+        </a>{" "}
+        , is open-sourced on{" "}
+        <a
+          href="https://github.com/JacyDee274/reactweatherappjackyfinal"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub{" "}
+        </a>
+        and hosted on{" "}
+        <a
+          href="https://jacky-weather-app-finale.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
-
