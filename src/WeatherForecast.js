9564a16ca6949c37d32acdc94f5d17cfb0506bcc
JacyDee274 <jacydee274@gmail.com>
2024-03-24 18:42:30 +0100

    Added latitude and longitude in forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,16 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon.js";
 import "./WeatherForecast.css";
+import Axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {}
+  let apiKey = "7746bdeabca928cfedcad71e52fd9d66";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordingates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  Axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
